a574841d937e0d300858d2c2918a0719e858d9c0
viresh18 <viresh.renukoot@gmail.com>
2021-03-07 18:20:02 +0530

    Created Basic Django Rest API for testing

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 
-import Prodcut from '../components/Product'
+import Product from '../components/Product'
 
 import products from '../products'
 
@@ -12,7 +12,7 @@ function HomeScreen() {
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Prodcut product={product} />
+                        <Product product={product} />
                     </Col>
                 ))}
             </Row>
