e61c7fdc4401d3b5b7bdea485676a4b268c2cf28
viresh18 <viresh.renukoot@gmail.com>
2021-03-10 19:50:37 +0530

    Bringing Redux Intro Home Screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,7 +3,7 @@ import {
     PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL 
 } from '../constants/productConstants'
 
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST})
 
